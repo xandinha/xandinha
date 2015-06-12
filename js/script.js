0aed9432b4864888fd97e375a923cfd8c171a0db
Junnior <junniordeveloper@gmail.com>
2015-06-12 09:21:04 -0700

    change layout, slide images and interviews

--- js/script.js
+++ js/script.js
@@ -1,1 +1,151 @@
-function smoothScroll(t){$('a[href^="#"]').on("click",function(e){var i=$($(this).attr("href"));i.length&&(e.preventDefault(),$("html, body").animate({scrollTop:i.offset().top},t))})}function workBelt(){$(".trigger").remove(),$(".return").remove(),$(".thumb-unit").click(function(){$(".work-belt").css("left","-100%"),$(".work-container").show()}),$(".work-return").click(function(){$(".work-belt").css("left","0%"),$(".work-container").hide(800)})}function workLoad(){$.ajaxSetup({cache:!0}),$(".thumb-unit").click(function(){var t=$(this),e=t.find("strong").text(),i=t.data("folder"),c='<div class="loader">Loading...</div>',l="work/"+i;$(".project-load").html(c).load(l),$(".project-title").text(e)})}function clientStuff(){$(".client-unit").first().addClass("active-client"),$(".client-logo").first().addClass("active-client"),$(".client-logo").click(function(){var t=$(this),e=t.parent().children(),i=e.index(t);$(".client-unit").removeClass("active-client").eq(i).addClass("active-client"),e.removeClass("active-client"),t.addClass("active-client")}),$(".client-control-next, .client-control-prev").click(function(){var t=$(this),e=$(".clients-belt").find(".active-client"),i=$(".clients-belt").children().index(e),c=$(".client-unit").length;t.hasClass("client-control-next")?c-1>i?$(".active-client").removeClass("active-client").next().addClass("active-client"):($(".client-unit").removeClass("active-client").first().addClass("active-client"),$(".client-logo").removeClass("active-client").first().addClass("active-client")):0===i?($(".client-unit").removeClass("active-client").last().addClass("active-client"),$(".client-logo").removeClass("active-client").last().addClass("active-client")):$(".active-client").removeClass("active-client").prev().addClass("active-client")})}$(function(){smoothScroll(1500),workBelt(),workLoad(),clientStuff()});
+$(function() {
+  smoothScroll(1500);
+  workBelt();
+  workLoad();
+  clientStuff();
+  
+//  $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+//  $(".biglink").fitText(1.5);
+  
+});
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+
+      var target = $( $(this).attr('href') );
+
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, duration);
+      }
+  });
+}
+
+
+function workBelt() {
+  
+  $(".trigger").remove();
+  $(".return").remove();
+
+  $('.thumb-unit').click(function() {
+    $('.work-belt').css('left','-100%');
+    $('.work-container').show();
+  });
+  
+  $('.work-return').click(function() {
+    $('.work-belt').css('left','0%');
+    $('.work-container').hide(800);
+  });
+
+}
+
+
+function  workLoad() {
+  
+  $.ajaxSetup({ cache: true });
+  
+  $('.thumb-unit').click(function() {
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newfolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = 'work/'+ newfolder;
+      
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
+  });
+  
+}
+
+
+function clientStuff() {
+  
+  $('.client-unit').first().addClass('active-client');
+  $('.client-logo').first().addClass('active-client');
+  //$('.clients-mobile-nav span').first().addClass('active-client');
+  
+  
+  $('.client-logo').click(function() {
+    var $this = $(this),
+        $siblings = $this.parent().children(),
+        position = $siblings.index($this);
+        
+    $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
+    $siblings.removeClass('active-client');
+    $this.addClass('active-client');
+  });
+  
+  
+  $('.client-control-next, .client-control-prev').click(function() {
+  
+    var $this = $(this),
+        curActiveClient = $('.clients-belt').find('.active-client'),
+        position = $('.clients-belt').children().index(curActiveClient),
+        clientNum = $('.client-unit').length;
+        
+      if($this.hasClass('client-control-next')) {
+        
+        if(position < clientNum -1){
+          $('.active-client').removeClass('active-client').next().addClass('active-client');
+        } else {
+          $('.client-unit').removeClass('active-client').first().addClass('active-client');
+          $('.client-logo').removeClass('active-client').first().addClass('active-client');
+        }
+        
+      } else {
+        
+        if (position === 0) {
+          $('.client-unit').removeClass('active-client').last().addClass('active-client');
+          $('.client-logo').removeClass('active-client').last().addClass('active-client');
+        } else {
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');  
+        }
+
+      }
+        
+  
+  });
+  
+}
+
+
+/*(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );*/
+
+
+
+
+
