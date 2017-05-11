f84c9986a9866409a9aeac1fb60844c83609fe47
junnior <junniordeveloper@gmail.com>
2017-05-11 09:24:53 -0300

    updated the old wensite

---
+++ js/function.js
@@ -0,0 +1,44 @@
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+
+  $('.parallax').css({
+    'transform' : 'translate(0px, -'+ wScroll /60 +'% )'
+  });
+
+  $('.back-bird').css({
+    'transform' : 'translate(0px,'+ wScroll /4 +'% )'
+  });
+
+  $('.fore-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll /40 +'% )'
+  });
+
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+  });
+
+  if (wScroll >= 100) {$(".logo_one").addClass("logo");} else {$(".logo_one").removeClass("logo");}
+
+  if (wScroll >= 100) {$('.logo_one').attr('src', 'img/logo/logo_xan_always_surfing_dark_gray.png');}
+     else {$('.logo_one').attr('src', 'img/logo/logo_xan_always_surfing_white_test_with_shadow_two.png');}
+
+  if (wScroll >= 100) {$(".navbar-default .nav > li>a").addClass("effect");} else{$(".navbar-default .nav > li>a").removeClass("effect");}   
+
+});
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
