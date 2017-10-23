5032b835be19de7f2eb1c187e00041f15256cbaf
junnior <junniordeveloper@gmail.com>
2017-10-23 11:00:57 -0200

    new upadated octuber 2017 change the big image from the end of the site to a many photos mosaic

---
+++ blog/renda-blog/index_pagination_test.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() { 
+
+
+    pageSize = 2;
+    pagesCount = $(".content").length;
+    var currentPage = 1;
+
+    /////////// PREPARE NAV ///////////////
+    var nav = '';
+    var totalPages = Math.ceil(pagesCount / pageSize);
+    for (var s=0; s<totalPages; s++){
+        nav += '<li class="numeros"><a href="#">'+(s+1)+'</a></li>';
+    }
+    $(".pag_prev").after(nav);
+    $(".numeros").first().addClass("active");
+    //////////////////////////////////////
+
+    showPage = function() {
+        $(".content").hide().each(function(n) {
+            if (n >= pageSize * (currentPage - 1) && n < pageSize * currentPage)
+                $(this).show();
+        });
+    }
+    showPage();
+
+
+    $(".pagination li.numeros").click(function() {
+        $(".pagination li").removeClass("active");
+        $(this).addClass("active");
+        currentPage = parseInt($(this).text());
+        showPage();
+    });
+
+    $(".pagination li.pag_prev").click(function() {
+        if($(this).next().is('.active')) return;
+        $('.numeros.active').removeClass('active').prev().addClass('active');
+        currentPage = currentPage > 1 ? (currentPage-1) : 1;
+        showPage();
+    });
+
+    $(".pagination li.pag_next").click(function() {
+        if($(this).prev().is('.active')) return;
+        $('.numeros.active').removeClass('active').next().addClass('active');
+        currentPage = currentPage < totalPages ? (currentPage+1) : totalPages;
+        showPage();
+    });
+});
